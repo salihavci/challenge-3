9a5d49311c0835152e5ccf2d6da322b07932f218
salih avcı <casus_salih_16@hotmail.com>
2024-04-12 22:20:28 +0300

    Akordiyon yapısı tekil açılacak şekilde düzenlendi ve children yapısı kullanıldı.

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import Item from "./Item";
 
 function Accordion({ items }) {
+  const [curOpen, setIsOpen] = useState(null);
+
   return (
     <div className="accordion">
       {items.map((x, index) => {
-        return <Item item={x} index={index} key={index} />;
+        return (
+          <Item
+            item={x}
+            index={index}
+            key={index}
+            curOpen={curOpen}
+            onOpen={setIsOpen}
+          >
+            {x.text}
+          </Item>
+        );
       })}
+      <Item
+        item={{ title: "Deneme 123" }}
+        index={22}
+        key={22}
+        curOpen={curOpen}
+        onOpen={setIsOpen}
+      >
+        <p>Deneme bir başlık</p>
+        <ul>
+          <li>Deneme 1</li>
+          <li>Deneme 2</li>
+          <li>Deneme 3</li>
+        </ul>
+      </Item>
     </div>
   );
 }
--- src/Item.js
+++ src/Item.js
@@ -1,18 +1,16 @@
-import { useState } from "react";
+function Item({ item, index, curOpen, onOpen, children }) {
+  const isOpen = index === curOpen;
 
-function Item({ item, index }) {
-  const [open, setOpen] = useState(false);
-
-  function handleSetOpen() {
-    setOpen(!open);
+  function handleToggle() {
+    onOpen(isOpen ? null : index);
   }
 
   return (
-    <div className={`item ${open && `open`}`} onClick={handleSetOpen}>
+    <div className={`item ${isOpen && `open`}`} onClick={handleToggle}>
       <p className="number">{index + 1 < 9 ? `0${index + 1}` : index + 1}</p>
       <p className="title">{item.title}</p>
-      <p className="icon">{open ? `-` : `+`}</p>
-      {open && <div className="content-box">{item.text}</div>}
+      <p className="icon">{isOpen ? `-` : `+`}</p>
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
